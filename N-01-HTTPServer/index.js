971ada6a839c438c4cae98a4fa515cd13fd0aa74
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-06-18 13:59:34 +0500

    HTTPMethods

--- N-01-HTTPServer/index.js
+++ N-01-HTTPServer/index.js
@@ -1,21 +1,27 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer( (req, res)=> {
 
-    if (req.url === '/favicon.ico') {
+    if (req.url === "/favicon.ico") {
         res.end();
+        return;
     }
 
     const log = `${(Date.now())}: ${req.url} New Request Received.\n`;
+    const myURL = url.parse(req.url, true);
+    console.log(myURL);
+
     fs.appendFile("log.txt", log, (error, data)=> {
 
-        switch(req.url) {
+        switch(myURL.pathname) {
             case '/':
                 res.end("Home");
                 break;
             case '/about':
-                res.end("About: I'm Danyal.");
+                const temp = myURL.query.username;
+                res.end(`Hi, ${temp}`);
                 break;
             default:
                 res.end("404 Page Not Found.");
