11c21de618c58fce71a06421093f9c078c383882
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-06-18 11:45:26 +0500

    npm URL

--- N-01-HTTPServer/index.js
+++ N-01-HTTPServer/index.js
@@ -3,8 +3,13 @@ const fs = require("fs");
 
 const myServer = http.createServer( (req, res)=> {
 
+    if (req.url === '/favicon.ico') {
+        res.end();
+    }
+
     const log = `${(Date.now())}: ${req.url} New Request Received.\n`;
     fs.appendFile("log.txt", log, (error, data)=> {
+
         switch(req.url) {
             case '/':
                 res.end("Home");
