78800b720bfee7ecf086ea88a0187f597bec440f
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-06-19 17:39:03 +0500

    done

---
+++ JS-12-Popup/script.js
@@ -0,0 +1,8 @@
+let popup = document.getElementById('popup');
+
+function showPopup() {
+    popup.classList.add("show-popup")
+}
+function hidePopup() {
+    popup.classList.remove("show-popup")
+}
