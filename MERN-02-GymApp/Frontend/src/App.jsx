7f5bca2cf0186ab7a77d48e85a2a6adefa16fc2a
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-07-06 01:24:43 +0500

    chore: Update Vite configuration and add React plugin

---
+++ MERN-02-GymApp/Frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Hero/>
+      <WorkoutSessions/>
+      <Gallery/>
+      <Pricing/>
+      <Contact/>
+      <BMICalculator/>
+      <Footer/>
+      <ToastContainer theme='dark' position='top-center' />
+    </Router>
+  )
+}
+
+export default App
