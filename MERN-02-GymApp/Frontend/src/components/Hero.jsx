1f466b80000a634b555fca86d370760c3fda67df
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-07-06 02:31:26 +0500

    chore: Add missing components and update file structure

---
+++ MERN-02-GymApp/Frontend/src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='hero'>
+      <div className="content">
+        <div className="title">
+          <h1>Lets's</h1>
+          <h1>Get</h1>
+          <h1>Moving</h1>
+        </div>
+        <div className="sub-title">
+          <p>Your journey to Fitness Starts here</p>
+          <p>Unleash your Potential</p>
+        </div>
+        <div className="buttons">
+          <button>Start the journey</button>
+          <button>Discover Plans</button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
+
+
---
+++ MERN-02-GymApp/Frontend/src/components/WorkoutSessions.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const WorkoutSessions = () => {
+  return (
+    <section className='workout_session'>
+      <div className="wrapper">
+        <h1>TOP WORKOUT SESSIONS</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates atque modi mollitia dolorum quam, aliquid laboriosam obcaecati vero.
+        </p>
+        <img src="/img5.jpg" alt="workout" />
+      </div>
+      <div className="wrapper">
+        <h1>FEATURED BOOTCAMPS</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis voluptas ipsum consequatur, amet ullam dignissimos numquam minima. Veniam?
+        </p>
+        <div className="bootcamps">
+          <div>
+            <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus consequatur maiores vitae voluptates consequuntur molestiae.</p>
+          </div>
+          <div>
+            <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus consequatur maiores vitae voluptates consequuntur molestiae.</p>
+          </div>
+          <div>
+            <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus consequatur maiores vitae voluptates consequuntur molestiae.</p>
+          </div>
+          <div>
+            <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus consequatur maiores vitae voluptates consequuntur molestiae.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default WorkoutSessions
