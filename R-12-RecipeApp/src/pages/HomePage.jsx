e2eca6a7019683e4fe3f3e81e980bb5cf3aad9ad
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-07-08 03:01:45 +0500

    feat: Add RecipeCard component and update HomePage layout

--- R-12-RecipeApp/src/pages/HomePage.jsx
+++ R-12-RecipeApp/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
-import { Heart, HeartPulse, Search, Soup } from 'lucide-react'
 import React from 'react'
+import RecipeCard from '../components/RecipeCard'
+import { Search } from 'lucide-react'
 
 const HomePage = () => {
     return (
@@ -15,35 +16,20 @@ const HomePage = () => {
                 <p className='text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight'>Popular Choices</p>
 
                 <div className='grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-                    {/* <RecipeCard /> */}
-                    <div className='flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-3 relative'>
-                        <a href="#" className='relative h-32'>
-                            <img src="/1.jpg" alt="image" className='rounded-md w-full h-full object-cover cursor-pointer' />
-                            <div className='absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm'>
-                                <Soup size={16} />
-                                <span>4 Servig</span>
-                            </div>
-                            <div className='absolute top-1 right-2 bg-white rounded-full p-1 cursor-pointer'>
-                                <Heart size={20} className='hover:fill-red-500 hover:text-red-500'/>
-                            </div>
-                        </a>
-
-                        <div className='flex mt-1'>
-                            <p className='font-bold tracking-wide'>Roasted Chicken</p>
-                        </div>
-                        <p className='my-2'>Turkish Chicken</p>
-
-                        <div className='flex gap-2 mt-auto'>
-                            <div className='flex gap-1 bg-[#d6f497] items-center p-1 rounded-md'>
-                                <Heart size={16} />
-                                <span className='text-sm tracking-tighter font-semibold'>Gluten-free</span>
-                            </div>
-                            <div className='flex gap-1 bg-[#d6f497] items-center p-1 rounded-md'>
-                                <HeartPulse size={16} />
-                                <span className='text-sm tracking-tighter font-semibold'>Heart-healthy</span>
-                            </div>
-                        </div>
-                    </div>
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    
                 </div>
             </div>
         </div>
