b8b12e8cf2e82e3b5a5858ab5381b90d1451fc96
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-07-10 18:38:53 +0500

    chore: Update page title and layout for Todo v1 app

--- R-13-Todo-v1/src/App.jsx
+++ R-13-Todo-v1/src/App.jsx
@@ -1,7 +1,23 @@
-function App () {
+import AppName from "./components/AppName"
+import AddTodo from "./components/AddTodo"
+import TodoItem1 from "./components/TodoItem1"
+import TodoItem2 from "./components/TodoItem2"
+import './App.css'
+
+function App() {
 
   return (
-    <div>PlaceHolder</div>
+    <center className='todo-container'>
+
+      <AppName />
+      <AddTodo />
+
+      <div className="items-container">
+        <TodoItem1 />
+        <TodoItem2 />
+      </div>
+
+    </center>
   )
 }
 
