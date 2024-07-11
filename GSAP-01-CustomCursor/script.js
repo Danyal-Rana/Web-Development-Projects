979751506e87b5f59adec83779dd1bce7c1d164a
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-07-11 16:04:39 +0500

    chore: Update GSAP-01-CustomCursor page layout and add lazy loading for images

--- GSAP-01-CustomCursor/script.js
+++ GSAP-01-CustomCursor/script.js
@@ -1,11 +1,28 @@
 const main = document.querySelector('#main');
 const cursor = document.querySelector('#cursor');
+const imageDiv = document.querySelector('#image');
 
 main.addEventListener('mousemove', (dets) => {
     gsap.to(cursor, {
         x: dets.x,
         y: dets.y,
-        duration: 0.5,
-        ease: "back.out"
+        duration: 1,
+        ease: "back.out",
+        
+    })
+})
+
+imageDiv.addEventListener('mouseenter', () => {
+    cursor.innerHTML = "View More";
+    gsap.to(cursor, {
+        scale:2,
+        backgroundColor: "#ffffff5c"
+    })
+})
+imageDiv.addEventListener('mouseleave', () => {
+    cursor.innerHTML = "";
+    gsap.to(cursor, {
+        scale:1,
+        backgroundColor: "#fff"
     })
 })
