7cec17becec0158222d2fd8fd2d97f3373ab47f9
Danyal Rana <ranadanyalarshad@gmail.com>
2024-08-02 01:23:38 +0500

    chore: Add react-router-dom package for client-side routing

--- R-16-ImageGeneration/client/src/App.jsx
+++ R-16-ImageGeneration/client/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import {logo} from './assets'
+import {Home, CreatePost, SinglePost} from './pages'
 
 
 const App = () => {
