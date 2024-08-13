edbce3c81793477e79b10d098a2c898c59dc9d72
Danyal Rana <ranadanyalarshad@gmail.com>
2024-08-14 02:47:23 +0500

    chore: Add error middleware to handle server errors

--- MERN-03-Portfolio1/backend/middlewares/error.js
+++ MERN-03-Portfolio1/backend/middlewares/error.js
@@ -5,6 +5,37 @@ class ErrorHandler extends Error {
     }
 };
 
+export const errorMiddleware = (err, req, res, next) => {
+    err.message = err.message || "Internal Server Error";
+    err.statusCode = err.statusCode || 500;
 
+    if (err.statusCode === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+        err = new ErrorHandler(message, 400);
+    }
+    if (err.name === "JsonWebTokenError") {
+        const message = "JSON Web Token is invalid. Try again!!!";
+        err = new ErrorHandler(message, 400);
+    }
+    if (err.name === "TokenExpiredError") {
+        const message = "JSON Web Token is expired. Try again!!!";
+        err = new ErrorHandler(message, 400);
+    }
+    if (err.name === "CastError") {
+        const message = `Resource not found. Invalid: ${err.path}`;
+        err = new ErrorHandler(message, 404);
+    }
+
+    const errorMessage = err.errors
+        ? Object.values(err.errors)
+            .map((error) => err.message)
+            .join(" ")
+        : err.message;
+
+    return res.status(err.statusCode).json({
+        success: false,
+        error: errorMessage
+    });
+};
 
 export default ErrorHandler;
