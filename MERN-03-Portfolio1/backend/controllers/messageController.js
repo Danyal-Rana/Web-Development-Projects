1a4947e7c8e3eefca7e03e464b987a76e8288409
Danyal Rana <ranadanyalarshad@gmail.com>
2024-08-17 07:13:58 +0500

    chore: Add CSS file for buttons and update index.html with button elements

---
+++ MERN-03-Portfolio1/backend/controllers/messageController.js
@@ -0,0 +1,19 @@
+import {catchAsyncErrors} from '../middlewares/catchAsyncErrors.js';
+import ErrorHandler from '../middlewares/error.js';
+import {Message} from '../models/messageModel.js';
+
+export const sendMessage = catchAsyncErrors(async (req, res, next) => {
+    const {senderName, subject, message} = req.body;
+
+    if (!senderName || !subject || !message) {
+        return next(new ErrorHandler('Please fill in all fields', 400));
+    }
+
+    const data = await Message.create({senderName, subject, message});
+
+    res.status(200).json({
+        success: true,
+        message: "Message Sent",
+        data,
+    });
+});
