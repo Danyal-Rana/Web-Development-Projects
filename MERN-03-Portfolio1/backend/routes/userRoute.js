3bf7a5ed8a504e02e80d6e6cc78e5f09d4eb4f41
Danyal Rana <ranadanyalarshad@gmail.com>
2024-08-19 18:53:23 +0500

    chore: Update userRoute.js to change logout route from POST to GET

--- MERN-03-Portfolio1/backend/routes/userRoute.js
+++ MERN-03-Portfolio1/backend/routes/userRoute.js
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', isAuthenticated, logout);
+router.get('/logout', isAuthenticated, logout);
 
 export default router;
