00d47f7bb014ff4a91652185615c396106db9e4f
Danyal-Rana <ranadanyalarshad@gmail.com>
2024-10-23 22:52:15 +0500

    refactor: Update .gitignore, backend/index.js, and package.json files

--- MERN-03-Auth/backend/index.js
+++ MERN-03-Auth/backend/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './db/connectDB.js';
 
 const app = express();
+dotenv.config();
 
 app.get('/', (req, res) => {
     res.send('Hello World from Node');
 });
 
 app.listen(3000, () => {
+    connectDB();
     console.log('Server is running on port 3000');
 });
